08de173dbe9d5e725c311664a40942ddf483d02c
gracebir <grace.devolop@gmail.com>
2022-10-28 09:35:07 +0200

    finish with header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import link from '../../assets/share_icon.png'
+import link_mobile from '../../assets/share_icon_mobile.png'
 import './header.css'
 
 function Header() {
@@ -7,6 +8,9 @@ function Header() {
     <header className='header'>
        <a className='share__link' href="#header">
         <img src={link} alt="" srcset="" />
+       </a>
+       <a className='mob_share' href="#header">
+        <img src={link_mobile} alt="" srcset="" />
        </a>
     </header>
   )
